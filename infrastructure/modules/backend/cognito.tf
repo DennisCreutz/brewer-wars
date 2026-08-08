bac59d89c189beb8a00d0183b2decf51f825a4a1
dennis.creutz <dennis.creutz@prodyna.com>
2026-08-08 23:39:28 +0200

    Lower Cognito password minimum length to 6 characters

--- infrastructure/modules/backend/cognito.tf
+++ infrastructure/modules/backend/cognito.tf
@@ -11,7 +11,7 @@ resource "aws_cognito_user_pool" "this" {
   auto_verified_attributes = ["email"]
 
   password_policy {
-    minimum_length    = 12
+    minimum_length    = 6
     require_lowercase = true
     require_uppercase = true
     require_numbers   = true
